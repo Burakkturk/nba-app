dec263a10b5d4476fc7968d33a4ffbf14d63129f
Burakkturk <burak1845turk@icloud.com>
2024-04-30 22:24:40 +0300

    first commit

---
+++ src/components/ContainerCard.jsx
@@ -0,0 +1,55 @@
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import PlayerCard from "./PlayerCard";
+import { useState } from "react";
+import { data } from "../helpers/data";
+
+const ContainerCard = () => {
+  const [search, setSearch] = useState("");
+  console.log(data);
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setSearch(e.target.value);
+    // console.log(search)
+  };
+  console.log(search);
+
+  const filteredData = data.filter((item) =>
+    item.name.toLowerCase().includes(search.trim().toLowerCase())
+  );
+
+  console.log("filter", filteredData);
+  return (
+    <>
+      <Form.Control
+        type="search"
+        placeholder="Search Player..."
+        onChange={handleChange}
+      />
+      <Container className="p-3 rounded-4 card-container my-3">
+        <Row xs={2} md={4} lg={6} className="justify-content-center g-3">
+          
+          {filteredData.map((player, i) => (
+            <PlayerCard
+              key={i}
+              player={player}
+              name={player.name}
+              img={player.img}
+              statistics={player.statistics}
+              {...player}
+            />
+          ))}
+          {filteredData.map((player, i) => (
+            <PlayerCard
+              key={i}
+              {...player}
+            />
+          ))}
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default ContainerCard;
---
+++ src/components/PlayerCard.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import Col from "react-bootstrap/Col";
+import Card from 'react-bootstrap/Card';
+
+const PlayerCard = ({name,img,statistics}) => {
+  return (
+    <Col>
+      <Card>
+        <Card.Img variant="top" src={img} height="350px" />
+        <ul>
+          {
+            statistics.map((item,i)=> <li key={i}>{item}</li>)
+          }
+        </ul>
+        <Card.Footer>
+        <Card.Title>{name}</Card.Title>
+        </Card.Footer>
+      </Card>
+    </Col>
+  )
+}
+
+export default PlayerCard
